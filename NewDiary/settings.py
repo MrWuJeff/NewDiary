462b2cfe103405b946b0012fe646b61edcefc40a
wujietian05@163.com <wujietian05@163.com>
2019-03-08 12:52:24 +0800

    修改setting文件,增加db连接,增加index页面,

--- NewDiary/settings.py
+++ NewDiary/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'NewDiary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_Notepad ',
+        'USER':'root',
+        'PASSWORD':'password',
+        'HOST':'119.23.204.8',
+        'PORT':'3306'
     }
 }
 
